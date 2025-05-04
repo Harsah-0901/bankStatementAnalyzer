f7c840b23f3af92d4b851d2fd63f8f05bb2ccff4
RohanK-coder <kyrohan7730@gmail.com>
2025-05-04 23:39:18 +0530

    dahboard-skeleton

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
-import { PrimaryButton } from '@fluentui/react';
+
 
 
 export default function Dashboard() {
   return (
-    <div>
-      
+    <div className='w-full h-screen border-2 border-red-500 flex flex-row'>
+      <div className='w-1/2 border-2 border-red-500 h-screen'>
+        <div className='w-full border-2 border-red-800 h-1/2'>Category wise spending summary</div>
+        <div className='w-full border-2 border-red-800 h-1/2'>Total Spending by Category</div>
+      </div>
+      <div className='w-1/2 border-2 border-red-500 h-screen'>
+        <div className='w-full border-2 border-red-800 h-1/2'>spending trend</div>
+        <div className='w-full border-2 border-red-800 h-1/2 flex-row flex'>
+          <div className='w-1/2 border-2 border-red-500 h-full'>Total Spending</div>
+          <div className='w-1/2 border-2 border-red-500 h-full'>top 5 transactions</div>
+        </div>
+      </div>
       
     </div>
   )
